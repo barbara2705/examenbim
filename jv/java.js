bd98d79aedecd1136eda7ec056c20b5da5a690a5
barbara2705 <barbarasamayaniperez@gmail.com>
2021-10-11 12:49:36 -0500

    examen

---
+++ jv/java.js
@@ -0,0 +1,55 @@
+let a = document.getElementById("a");
+let s = document.getElementById("s");
+let d = document.getElementById("d");
+let f = document.getElementById("f");
+let g = document.getElementById("g");
+let h = document.getElementById("h");
+let j = document.getElementById("j");
+let k = document.getElementById("k");
+let l = document.getElementById("l");
+let sna = document.getElementById("sna");
+let sns = document.getElementById("sns");
+let snd = document.getElementById("snd");
+let snf = document.getElementById("snf");
+let sng = document.getElementById("sng");
+let snh = document.getElementById("snh");
+let snj = document.getElementById("snj");
+let snk = document.getElementById("snk");
+let snl = document.getElementById("snl");
+let controls = document.querySelector(".controls");
+a.addEventListener("click", () =>{
+    sna.play();
+    controls.animationPlayState ="running";
+});
+s.addEventListener("click", () =>{
+    sns.play();
+    controls.animationPlayState ="running";
+});
+d.addEventListener("click", () =>{
+    snd.play();
+    controls.animationPlayState ="running";
+});
+f.addEventListener("click", () =>{
+    snf.play();
+    controls.animationPlayState ="running";
+});
+g.addEventListener("click", () =>{
+    sng.play();
+    controls.animationPlayState ="running";
+});
+h.addEventListener("click", () =>{
+    snh.play();
+    controls.animationPlayState ="running";
+});
+j.addEventListener("click", () =>{
+    snj.play();
+    controls.animationPlayState ="running";
+});
+k.addEventListener("click", () =>{
+    snk.play();
+    controls.animationPlayState ="running";
+});
+l.addEventListener("click", () =>{
+    snl.play();
+    controls.animationPlayState ="running";
+});
